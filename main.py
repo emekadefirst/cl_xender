071125e0064ecd372836eea6e9966eeee6709eec
Olayiwolaaa <muizzkara91@gmail.com>
2024-01-20 15:15:31 +0100

    modified main.py and sender.py files

--- main.py
+++ main.py
@@ -4,4 +4,18 @@ from recipient import listener
 print("What action do you want to perform?")
 response = input("Enter your choice (send/receive): ")
 
-server() if response == "send" else listener() if response == "receive" else print("Invalid request")
+if response == "send":
+    # Get user input for server IP and port
+    server_ip = input("Enter server IP: ")
+    server_port_num = input("Enter server port: ")
+
+    # Use default port 54321 if the user didn't provide a port
+    server_port = 54321 if server_port_num == '' else int(server_port_num)
+
+    # Display the default port message only when the default port is used
+    if server_port == 54321:
+        print('Using default port: 54321')
+    server(server_ip, server_port)
+elif response == "receive":
+    listener()
+else: print("Invalid request")
